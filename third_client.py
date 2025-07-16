9ec260f443297167f873c63dc8d43cc12c70ecec
DonatasNoreika <donoras@gmail.com>
2025-07-16 21:00:13 +0300

    Sukurtas taskų trynimas/redagavimas

--- third_client.py
+++ third_client.py
@@ -28,6 +28,25 @@ def new_task():
         return redirect("/tasks")
     return render_template("task_new.html")
 
+@app.route("/tasks/<int:id>/delete", methods=["GET"])
+def delete_task(id):
+    r = requests.delete(f"http://127.0.0.1:5000/uzduotys/{id}")
+    return redirect("/tasks")
+
+
+@app.route("/tasks/<int:id>/edit", methods=["GET", "POST"])
+def edit_task(id):
+    if request.method == "POST":
+        payload = {
+            "pavadinimas": request.form['title'],
+            "atlikta": False
+        }
+        r = requests.put(f"http://127.0.0.1:5000/uzduotys/{id}", json=payload)
+        return redirect("/tasks")
+    if request.method == "GET":
+        r = requests.get(f"http://127.0.0.1:5000/uzduotys/{id}")
+        task = r.json()
+        return render_template("task_edit.html", task=task)
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
